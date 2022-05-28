906cd952d68a760f59c8cd506b9282a08d11a706
Anup81 <anupdhakal81@gmail.com>
2022-05-28 10:14:56 +0545

    React With Socket IO

---
+++ server/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+const http = require("http");
+const { Server } = require("socket.io");
+
+app.use(cors());
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`User Connected: ${socket.id}`);
+
+  socket.on("send_message", (data) => {
+    socket.broadcast.emit("receive_message", (data) => {
+      alert(data.message);
+    });
+  });
+});
+
+server.listen(3001, () => {
+  console.log("Server is Running");
+});
